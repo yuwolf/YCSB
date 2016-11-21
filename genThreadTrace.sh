55e917bad0a20d9d2dc6338701b713cf691f8fc2
mingfire <mingfire@mail.ustc.edu.cn>
2016-11-21 16:02:17 +0800

    rm genThreadTrace.sh

--- genThreadTrace.sh
+++
@@ -1,5 +0,0 @@
-RECORDCOUNT=100
-FIELDLENGTH=100
-WORKLOAD=workloada
-bin/ycsb load basic -P workloads/"$WORKLOAD" -p recordcount="$RECORDCOUNT" -p fieldlength="$FIELDLENGTH" -p fieldcount=1 -threads 1
-#bin/ycsb run basic -P workloads/"$WORKLOAD" -p recordcount="$RECORDCOUNT" -p operationcount="$RECORDCOUNT" -p fieldlength="$FIELDLENGTH" -p fieldcount=1
